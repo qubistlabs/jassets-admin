efe30776a8b7759e9c524da50419d1e20a2cd5e9
Roman Tolkachyov <roman@tolkachyov.name>
2019-10-16 14:31:55 +0300

    serve static using python

--- jassets_admin/urls.py
+++ jassets_admin/urls.py
@@ -1,8 +1,17 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
 from django.views.generic import RedirectView
+from django.views.static import serve
+from django.conf import settings
 
 urlpatterns = [
     path('', RedirectView.as_view(url='admin/')),
     path('admin/', admin.site.urls),
 ]
+
+if not settings.DEBUG:
+    urlpatterns += [
+        re_path(r'^static/(?P<path>.*)$', serve, {
+            'document_root': settings.STATIC_ROOT,
+        }),
+    ]
